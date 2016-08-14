c53d73ce859c4287f534892b0cf8d749ec259daa
kareningenuity <karenjxliu@hotmail.com>
2016-08-14 11:38:00 -0500

    added push function for tasks and reviews to add to the array temporarily

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -16,6 +16,15 @@
       "I don't like AngularJS but I do like it."
     ];
 
+   $scope.addTask = function(input) {
+      $scope.tasks.push(input);
+    };    
+
+   $scope.addReview = function(inputReview) {
+    // Doesn't have to be called inputreview. it's just the name of the argument.
+      $scope.reviews.push(inputReview);
+    };
+
     window.$scope = $scope;    
   });
 })();
