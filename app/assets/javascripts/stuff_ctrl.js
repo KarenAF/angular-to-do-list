5879fdfa54100d5f0ed8d86c451154a12eeb0049
kareningenuity <karenjxliu@hotmail.com>
2016-08-15 21:38:57 -0500

    fixed the silent failing of the ng-click

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -8,24 +8,28 @@
       "Feed the cat",
       "Feed the baby",
       "Feed the car"
-    ]
+    ];
+
     $scope.reviews = [
       "AngularJS is awesome.",
       "AngularJS is weird",
       "I don't like AngularJS but I do like it."
     ];
 
-   $scope.newTask = ""; 
-
-   $scope.addTask = function(input) {
-    if(input !== "") {
-      $scope.tasks.push(input);
+    $scope.addTask = function(input) {
+      if(input !== "") {
+        $scope.tasks.push(input);
       };
-    $scope.newTask = ""  
+      $scope.newTask = ""  
     };    
 
-   $scope.addReview = function(inputReview) {
-    // Doesn't have to be called inputreview. it's just the name of the argument.
+    $scope.completeTask = function(inputIndex) {
+      // $scope.tasks.splice(inputIndex, 1);
+      console.log("Hi");
+    };
+
+    $scope.addReview = function(inputReview) {
+      // Doesn't have to be called inputreview. it's just the name of the argument.
       $scope.reviews.push(inputReview);
     };
 
